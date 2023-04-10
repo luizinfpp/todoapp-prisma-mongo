e3e107ac549cc0b62986d60acace8d103daf97fd
luizinfpp <luizinfpp@gmail.com>
2023-04-10 13:47:42 -0300

    commit

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
 import express from "express"
+import cors from "cors"
+import helmet from "helmet"
 
 const app = express()
+
+app.use(cors())
+app.use(helmet())
 
 app.listen(8007, () => { console.log("Listening!!!") })
