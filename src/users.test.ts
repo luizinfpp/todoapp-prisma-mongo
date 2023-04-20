b9edc253eade69501fc924a0e8c3611e1830b37a
luizinfpp <luizinfpp@gmail.com>
2023-04-19 23:42:20 -0300

    commit w errors

---
+++ src/users.test.ts
@@ -0,0 +1,58 @@
+import { describe, test, expect  } from "vitest"
+
+const isDev = process.env.NODE_ENV === 'development'
+
+describe('users class unit tests', () => {
+
+    const name = 'test name'
+    const newName = 'new test name'
+
+    const user = new User()
+    let user2
+
+    test('constructor', () => {
+        
+        user2 = new User({ name: newName })
+
+        expect(user2.isEmpty()).toBeFalsy()
+        expect(user2.getName()).toEqual(newName)
+    })
+
+    test('create user and get name', () => {
+        
+        user.create({ name: name })
+
+        expect(user.isEmpty()).toBeFalsy()
+        expect(user.getName()).toEqual(name)
+    })
+
+    test('fetch user lists', () => {
+        let lists = user.fetchLists()
+
+        expect(lists).toBeInstanceOf(List[])
+    })
+
+    test('delete user', () => {
+        user2.delete({ name: newName })
+
+        expect(user2.isEmpty()).toBeTruthy()
+    })
+
+    test('set new name', () => {
+        user.setName({ name: newName })
+
+        expect(user.getName()).toEqual(newName)
+    })
+
+    test('get user', () => {
+        user2.get({ name: newName })
+
+        expect(user2.isEmpty()).toBeFalsy()
+    })
+
+})
+
+describe('users database tests', () => {
+
+
+})
